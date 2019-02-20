2a435249f1fb4f318f6734df61e31ce19955002b
Pablo Martínez Domingo <pablokzx@gmail.com>
2019-02-20 12:24:17 +0100

    Primeras pruebas

--- CodiPokedex/src/pruebas.js
+++ CodiPokedex/src/pruebas.js
@@ -1,8 +1,23 @@
-function setup(){
-    createCanvas(600, 600)
+var x = 0;
+var y = 0;
+var data;
+var pokemons;
+
+function preload(){  
+    data = loadJSON('assets/data/pokemon.json'); 
 }
 
-function draw(){
+function setup() {
+    pokemons = data.pokemon;
+
+    createCanvas(200, pokemons.length * 15);
     background(255)
-    ellipse(100, 100, 20)
-}
+
+    for (var i = 0; i < pokemons.length; i++) {   
+        var name = pokemons[i].name;
+        console.log(name);   
+        text(name, 0, i * 15);
+    }
+}   
+
+
