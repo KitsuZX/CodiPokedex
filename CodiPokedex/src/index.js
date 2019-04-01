916c173e3954edd1d7dff424cb3e7492bd5dbabe
DDChetok <43351928+DDChetok@users.noreply.github.com>
2019-04-01 13:11:20 +0200

    No se ni que hago.exe

--- CodiPokedex/src/index.js
+++ CodiPokedex/src/index.js
@@ -1,6 +1,8 @@
 var CodiPokedex = CodiPokedex || {};
 CodiPokedex.game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.CANVAS, 'game');
 
+var requirejs = require('requirejs');
+var MongoClient = require('mongoose');
 //States
 CodiPokedex.game.state.add('Boot', CodiPokedex.Boot);
 CodiPokedex.game.state.add('Preload', CodiPokedex.Preload);
@@ -8,5 +10,6 @@ CodiPokedex.game.state.add('Searcher', CodiPokedex.Searcher);
 CodiPokedex.game.state.add('Stats', CodiPokedex.Stats);
 
 CodiPokedex.game.state.start('Boot');
+
 
 
